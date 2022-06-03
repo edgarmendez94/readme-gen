5bafb61b5a0321aa130eb4f873f86d980f8a46c0
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-02 23:15:13 -0400

    added req markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -56,7 +57,7 @@ fs.writeFile("README.md", generateMarkdown() , (err) => {
 // // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions)
-
+  
 
 }
 
